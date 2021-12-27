8540eac857d59721cf4e5c224971b75db34ad8c0
Ryan Manny <ryan.w.manny@gmail.com>
2021-12-27 01:08:59 -0800

    Handle Roger case

--- roger.js
+++ roger.js
@@ -1,1 +1,4 @@
-document.getElementsByClassName('byline').item(0).textContent = "Roger Ebert's Ghost";
+const byline = document.getElementsByClassName('byline').item(0);
+if (byline.textContent.trim() !== "Roger Ebert") {
+    byline.textContent = "Roger Ebert's Ghost";
+};
